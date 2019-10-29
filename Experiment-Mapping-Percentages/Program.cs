650e40e92359627ec8882506ebdb386ab98da303
airbusramo <omarrefaat15@live.com>
2019-10-29 03:10:23 +0200

    Added methods Invert, Restart, and End.

--- Experiment-Mapping-Percentages/Program.cs
+++ Experiment-Mapping-Percentages/Program.cs
@@ -16,8 +16,84 @@ namespace Experiment_Mapping_Percentages
             double min = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("Enter Maximum Value:");
             double max = Convert.ToDouble(Console.ReadLine());
+            bool invert = Invert();
+
+            Console.WriteLine(min);
+            Console.WriteLine(max);
+            Console.WriteLine(invert);
+            Restart();
+
+        }
+
+        //Method for restarting the program.
+        private static bool Invert()
+        {
+            Console.WriteLine();
             Console.WriteLine("Invert? (y/n)");
 
+            try
+            {
+                char invertInput = Convert.ToChar(Console.ReadLine());
+
+                switch (char.ToLower(invertInput))
+                {
+                    case 'y':
+                        return true;
+                    case 'n':
+                        return false;
+                    default:
+                        Console.WriteLine("Invaild Invert Input");
+                        return Invert();
+                }
+            }
+
+            catch (System.FormatException)
+            {
+                Console.WriteLine("Invaild Invert Input");
+                return Invert();
+            }
+        }
+
+        //Method for restarting the program.
+        private static void Restart()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Restart? (y/n):");
+
+            try
+            {
+                char restartInput = Convert.ToChar(Console.ReadLine());
+
+                switch (char.ToLower(restartInput))
+                {
+                    case 'y':
+                        Main();
+                        break;
+                    case 'n':
+                        End();
+                        break;
+                    default:
+                        Console.WriteLine("Invaild Input");
+                        Restart();
+                        break;
+                }
+            }
+
+            catch (System.FormatException)
+            {
+                Console.WriteLine("Invaild Input");
+                Restart();
+            }
+
+        }
+
+        //Method for ending the program.
+        private static void End()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Press any key to close the program...");
+            Console.ReadKey();
+            System.Environment.Exit(1);
         }
     }
 }
