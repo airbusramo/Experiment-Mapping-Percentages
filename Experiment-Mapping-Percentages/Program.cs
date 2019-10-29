2959bf554b964cda46abaf38d9caad09996b6a36
airbusramo <omarrefaat15@live.com>
2019-10-29 02:05:16 +0200

    Add project files.

---
+++ Experiment-Mapping-Percentages/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Experiment_Mapping_Percentages
+{
+    class Program
+    {
+        static void Main()
+        {
+            //Inputs
+            Console.WriteLine("This is an experiment to see if my mapping idea will work.");
+            Console.WriteLine("Enter Minimum Value:");
+            double min = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter Maximum Value:");
+            double max = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Invert? (y/n)");
+
+        }
+    }
+}
